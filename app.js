487154ef39975083f3d0ebee4301c5c19d60a00d
nicolasclem <clementinicolas@gmail.com>
2021-05-01 09:05:29 -0300

    modificado para la entrega

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.resolve(__dirname,'public')));
 
 
 /*levanto el server*/
-app.listen(3030,()=>console.log("exito en levantar el servidor 3030"))
+app.listen(process.env.PORT|| 3000,()=>console.log("exito en levantar el servidor 3000"))
 
 /*rutas*/
 app.get('/', (req, res) => {
