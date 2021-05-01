bd792d8d2eff9b4444f76591a773ceae0299e2bd
nicolasclem <clementinicolas@gmail.com>
2021-05-01 02:03:43 -0300

    ultimo  commit

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require(`express`);
 const app = express();
 const path = require('path');
 
-
+/*carpeta estatica */
 app.use(express.static(path.resolve(__dirname,'public')));
 
+
+/*levanto el server*/
 app.listen(3030,()=>console.log("exito en levantar el servidor 3030"))
 
-
+/*rutas*/
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, `./views/home.html`))
 
